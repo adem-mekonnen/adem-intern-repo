b7cd7c634cee7df064c3e2d4b8f0198d2cbcda16
Adem Mekonnen <adem.se2010@gmail.com>
2026-07-31 16:53:42 +0300

    Add power function on conflict-demo branch

--- math.js
+++ math.js
@@ -3,3 +3,4 @@ function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) { return a * b; }
 console.log('math module loaded');
+function power(a, b) { return a ** b; }
